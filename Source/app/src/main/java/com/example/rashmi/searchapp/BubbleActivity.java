a878f014a61269cf5c6525826231d4b8595c4adf
ManasaReddyThipparthi <manasareddy.thipparthi@gmail.com>
2016-09-24 10:38:02 +0530

    Bubble Translation
    Commiting Bubble Translation

--- Source/app/src/main/java/com/example/rashmi/searchapp/BubbleActivity.java
+++ Source/app/src/main/java/com/example/rashmi/searchapp/BubbleActivity.java
@@ -5,25 +5,96 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
 
-public class BubbleActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bubble);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        final ImageView img_animation = (ImageView) findViewById(R.id.imageView);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+
+     //   img_animation.setOnTouchListener(new OnTouchListener() {
+
+
+       //     public boolean onTouch(View v) {
+          //      Log.v("blah", "blah blah");
+                //  v.setVisibility(View.GONE);
+                //  img_animation.setVisibility(View.GONE);
+                //  img_animation.setImageDrawable(null);
+                //  img_animation.setImageBitmap(null);
+             //    img_animation.setImageResource(android.R.color.transparent);
+               // img_animation.setImageResource(0);
+            //    return true;
+           // }
+       // });
+
+        TranslateAnimation tAnimation = new TranslateAnimation(0, 0, 4000, -1950);
+        tAnimation.setDuration(7000);
+        tAnimation.setRepeatCount(100);
+        tAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
+        tAnimation.setFillAfter(true);
+        tAnimation.setAnimationListener(new Animation.AnimationListener() {
+
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onAnimationStart(Animation animation) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+                // TODO Auto-generated method stub
+                img_animation.setOnTouchListener(new View.OnTouchListener() {
+                    public boolean onTouch(View v, MotionEvent ev) {
+                        img_animation.setVisibility(View.GONE);
+                        return true;
+                    }
+                });
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                // TODO Auto-generated method stub
             }
         });
+
+        img_animation.startAnimation(tAnimation);
+
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
+
