05d332bef503572c77e6e912363570a026a6c8bc
ManasaReddyThipparthi <manasareddy.thipparthi@gmail.com>
2016-11-09 22:27:05 +0530

    commit js

--- sourceionic/bubbleSmashApp/www/js/controllers.js
+++ sourceionic/bubbleSmashApp/www/js/controllers.js
@@ -1,102 +1,190 @@
-angular.module('starter.controllers', [])
-
-
-.controller('BubbleController', function($scope, Chats) {
-    $scope.easyLevel = function(){
-        $state.go('easyLevel')        
-    }
-    $scope.intermediateLevel = function(){
-        $state.go('intermediateLevel')        
-    }
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller("BubbleController", function($scope, $state, $window) {
-   
-    var init = function () {
-        alert("eeasy");
-        $scope.score=0;
-    };
-    init();
-    // Easy Level
+bubbleSmashApp.controller("ScoreController", function($scope, $state, dataService) {
+    $scope.data = dataService.dataObj;
     
-    var bubbleIcon = document.getElementById("bubbleIcon"); 
-    var bubble = document.getElementById("bubble"); 
-    setInterval(function(){ $state.go('scorepage') }, 100000);
-    bubbleIcon.style.display = 'block'; 
-    $scope.easylevelcount = 0;
-    $scope.loop = function(){
-        var offsets = document.getElementById('bubble').getBoundingClientRect();
-        var top = offsets.top;
-        bubbleIcon.style.display = 'block';
-        bubble.style.marginTop = -top - 500 + 'px';
+    $scope.backToHome = function() {
+        $state.go('backtohome')
     }
-    $scope.smashBubble = function(){
-        $scope.easylevelcount = $scope.easylevelcount + 1;
-        bubbleIcon.style.display = 'none';  
-        setTimeout(function(){
-            $scope.loop();
-        },100);
-        $scope.score =  $scope.easylevelcount;
-    } 
-    var score = function (){
-        alert("score"+ $scope.score);
-        $scope.score = $scope.score;
+    
+    $scope.quit = function() {
+        $state.go('quit')
     }
-    score();
-})
+});
+bubbleSmashApp.controller("LoginController", function($scope, $http, $state, $window) {
 
-bubbleSmashApp.controller("IntermediateController", function($scope, $state) {    
-    var bubbleIcon1 = document.getElementById("bubbleIcon1"); 
-    var bubbleIcon2 = document.getElementById("bubbleIcon2"); 
-    var bubbleIcon3 = document.getElementById("bubbleIcon3"); 
-    var bubble1 = document.getElementById("bubble1"); 
-    var bubble2 = document.getElementById("bubble2");
-    var bubble3 = document.getElementById("bubble3"); 
-    bubbleIcon1.style.display = 'block';  
-    bubbleIcon2.style.display = 'block'; 
-    bubbleIcon3.style.display = 'block'; 
-    $scope.loop1 = function(){
-        bubbleIcon1.style.display = 'block';
-        bubble1.style.top = '-200px';
-        bubble1.style.position = 'absolute';
+    $scope.login = function(username, password) {
+      $http({
+        method: 'GET',
+        url : 'https://api.mlab.com/api/1/databases/asedb/collections/demoase?apiKey=s2gDY_UX42GDk8QBsJSlGuQqrwXFGtxg'
+
+      }).success(function(data) {
+            var obj=angular.fromJson(data);
+            var count=0;
+            for(i=0;i<obj.length;i++)
+            {
+                if (angular.equals(obj[i].username, username)&&angular.equals(obj[i].password,password)) {
+                    $state.go('home')
+                }
+                else {
+                     count++;
+                }
+            }
+             if(count==obj.length){
+                $scope.msg ="Login Unsuccessful";
+             }
+      })
     }
-    $scope.loop2 = function(){
-        bubbleIcon2.style.display = 'block';
-        bubble2.style.top = '-200px';
-        bubble2.style.position = 'absolute';
+    $scope.register = function(){
+        $state.go('register')
     }
-    $scope.loop3 = function(){
-        bubbleIcon3.style.display = 'block';
-        bubble3.style.top = '-200px';
-        bubble3.style.position = 'absolute';
+});
+bubbleSmashApp.controller("RegisterController", function($scope, $http, $state, $window) {
+     $scope.registerdata = function(name, username, password) {
+
+       $http({
+         method: 'POST',
+         url : 'https://api.mlab.com/api/1/databases/asedb/collections/demoase?apiKey=s2gDY_UX42GDk8QBsJSlGuQqrwXFGtxg',
+         data: JSON.stringify({
+           name : name,
+           username: username,
+           password: password
+         }),
+         contentType: "application/json"
+       }).success(function() {
+         $scope.status ="User created successfully";
+       }).error(function(data){
+         $scope.status = "User Cannot be Created";
+       })
+
+     }
+      
+    $scope.cancel = function(){
+        $state.go('cancel')
     }
+   
+});
+bubbleSmashApp.controller("LevelController", function($scope, $http, $state, $window) {
+     $scope.easylevel = function(){
+        $state.go('easylevel')
+    }
+
+    $scope.intermediatelevel = function() {
+        $state.go('intermediatelevel')
+    }
+
+      $scope.settings = function() {
+        $state.go('settings')
+    }
+
+    $scope.help = function() {
+        $state.go('help')
+    }
+});
+bubbleSmashApp.controller("EasyLevelController", function($scope, $state, dataService) {
+
+    // Easy Level
+    $scope.count = 0;
+    setInterval(function(){ $state.go('scorepage') }, 50000);
+    $scope.smashBubble1 = function(){
+        $scope.count = $scope.count + 1;
+        document.getElementById('bubble1').classList.remove('animation');
+        document.getElementById("bubble1").style.opacity = "0";
+        setTimeout(
+        function() {
+            document.getElementById('bubble1').classList.add('animation');
+            document.getElementById("bubble1").style.opacity = "1";
+        }, 100);
+        dataService.dataObj=$scope.count;
+        $scope.data = dataService.dataObj;
+    }
+    
+    $scope.smashBubble2 = function(){
+        $scope.count = $scope.count + 1;
+        document.getElementById('bubble2').classList.remove('animation');
+        document.getElementById("bubble2").style.opacity = "0";
+        setTimeout(
+        function() {
+            document.getElementById('bubble2').classList.add('animation');
+            document.getElementById("bubble2").style.opacity = "1";
+        }, 100);
+        dataService.dataObj=$scope.count;
+        $scope.data = dataService.dataObj;
+    }
+    $scope.smashBubble3 = function(){
+        $scope.count = $scope.count + 1;
+        document.getElementById('bubble3').classList.remove('animation');
+        document.getElementById("bubble3").style.opacity = "0";
+        setTimeout(
+        function() {
+            document.getElementById('bubble3').classList.add('animation');
+            document.getElementById("bubble3").style.opacity = "1";
+        }, 100);
+        dataService.dataObj=$scope.count;
+        $scope.data = dataService.dataObj;
+    }
+});
+    // Intermediate Level
+bubbleSmashApp.controller("IntermediateLevelController", function($scope, $state, dataService) {
+    
+    $scope.count = 0;
+    setInterval(function(){ $state.go('scorepage') }, 50000);
     $scope.smashBubbleIntermediate1 = function(){
-        bubbleIcon1.style.display = 'none';  
-        setTimeout(function(){
-            $scope.loop1();
-        },100)
+        $scope.count = $scope.count + 1;
+        document.getElementById('bubbleIntermediate1').classList.remove('animation');
+        document.getElementById("bubbleIntermediate1").style.opacity = "0";
+        setTimeout(
+        function() {
+            document.getElementById('bubbleIntermediate1').classList.add('animation');
+            document.getElementById("bubbleIntermediate1").style.opacity = "1";
+        }, 100);
+        dataService.dataObj=$scope.count;
+        $scope.data = dataService.dataObj;
     }
     $scope.smashBubbleIntermediate2 = function(){
-        bubbleIcon2.style.display = 'none';  
-        setTimeout(function(){
-            $scope.loop2();
-        },100)
+          $scope.count = $scope.count + 1;
+        document.getElementById('bubbleIntermediate2').classList.remove('animation');
+        document.getElementById("bubbleIntermediate2").style.opacity = "0";
+        setTimeout(
+        function() {
+            document.getElementById('bubbleIntermediate2').classList.add('animation');
+            document.getElementById("bubbleIntermediate2").style.opacity = "1";
+        }, 100);
+        dataService.dataObj=$scope.count;
+        $scope.data = dataService.dataObj;
     }
     $scope.smashBubbleIntermediate3 = function(){
-        bubbleIcon3.style.display = 'none';  
-        setTimeout(function(){
-            $scope.loop3();
-        },100)
+          $scope.count = $scope.count + 1;
+        document.getElementById('bubbleIntermediate3').classList.remove('animation');
+        document.getElementById("bubbleIntermediate3").style.opacity = "0";
+        setTimeout(
+        function() {
+            document.getElementById('bubbleIntermediate3').classList.add('animation');
+            document.getElementById("bubbleIntermediate3").style.opacity = "1";
+        }, 100);
+        dataService.dataObj=$scope.count;
+        $scope.data = dataService.dataObj;
     }
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-    
+    $scope.smashBubbleIntermediate4 = function(){
+          $scope.count = $scope.count + 1;
+        document.getElementById('bubbleIntermediate4').classList.remove('animation');
+        document.getElementById("bubbleIntermediate4").style.opacity = "0";
+        setTimeout(
+        function() {
+            document.getElementById('bubbleIntermediate4').classList.add('animation');
+            document.getElementById("bubbleIntermediate4").style.opacity = "1";
+        }, 100);
+        dataService.dataObj=$scope.count;
+        $scope.data = dataService.dataObj;
+    }
+    $scope.smashBubbleIntermediate5 = function(){
+          $scope.count = $scope.count + 1;
+        document.getElementById('bubbleIntermediate5').classList.remove('animation');
+        document.getElementById("bubbleIntermediate5").style.opacity = "0";
+        setTimeout(
+        function() {
+            document.getElementById('bubbleIntermediate5').classList.add('animation');
+            document.getElementById("bubbleIntermediate5").style.opacity = "1";
+        }, 100);
+        dataService.dataObj=$scope.count;
+        $scope.data = dataService.dataObj;
+    }
 });
